b9ad8de1bb2eb99cd8ea0909924eeee3a6ef69c3
xov121 <x.o.v121@gmail.com>
2024-04-26 12:19:00 -0400

    Testing code

---
+++ python_async_function/1-concurrent_coroutines.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+"""
+This module extends the asynchronous capabilities to execute multiple
+coroutines concurrently and return sorted delays.
+"""
+
+
+import asyncio
+from 0-basic_async_syntax import wait_random
+
+async def wait_n(n: int, max_delay: int) -> list:
+    """
+    Execute multiple instances of wait_random concurrently.
+    """
+    tasks = [asyncio.create_task(wait_random(max_delay)) for _ in range(n)]
+    delays = []
+    for task in asyncio.as_completed(tasks):
+        result = await task
+        delays.append(result)
+    return delays
