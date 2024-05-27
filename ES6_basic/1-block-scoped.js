99925b3e3f775a0c416796865f06454613680107
xov121 <x.o.v121@gmail.com>
2024-05-27 06:30:06 -0400

    Task 1

--- ES6_basic/1-block-scoped.js
+++ ES6_basic/1-block-scoped.js
@@ -1,10 +1,9 @@
 export default function taskBlock(trueOrFalse) {
-  let task = false;
-  let task2 = true;
+  const task = false;
+  const task2 = true;
 
   if (trueOrFalse) {
-    task = true;
-    task2 = false;
+    //    passing-lint
   }
 
   return [task, task2];
