9afff9c49fcfbf26e0dea58f065bd9fc3c3e2bc8
xov121 <x.o.v121@gmail.com>
2024-05-27 09:08:16 -0400

    Task 6

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -1,12 +1,14 @@
+#!/usr/bin/node
+
 import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
 async function handleProfileSignup(firstName, lastName, fileName) {
   const promise1 = {
-    status: 'pending ',
+    status: 'pending',
   };
   const promise2 = {
-    status: 'pending ',
+    status: 'pending',
   };
 
   try {
