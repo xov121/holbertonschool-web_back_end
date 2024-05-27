16f66ca1992329c4eda0c84505949b3a1e510bea
xov121 <x.o.v121@gmail.com>
2024-05-27 09:35:36 -0400

    Task 1

---
+++ ES6_data_manipulation/1-get_list_student_ids.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+
+function getListStudentIds(array) {
+  if (!Array.isArray(array)) {
+    return [];
+  }
+  return array.map(student => student.id);
+}
+
+export default getListStudentIds;
