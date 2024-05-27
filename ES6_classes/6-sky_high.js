81f6b1e20c9aea60034711a711760fef7d1bb5b8
xov121 <x.o.v121@gmail.com>
2024-05-27 07:38:16 -0400

    Task 6

--- ES6_classes/6-sky_high.js
+++ ES6_classes/6-sky_high.js
@@ -1,6 +1,6 @@
 import Building from './5-building';
 
-export default class SkyHighBuilding extends Building {
+class SkyHighBuilding extends Building {
   constructor(sqft, floors) {
     super(sqft);
     this._floors = floors;
@@ -11,6 +11,8 @@ export default class SkyHighBuilding extends Building {
   }
 
   evacuationWarningMessage() {
-    return `Evacuate slowly the ${this._floors} floors.`;
+    return `Evacuate slowly the ${this._floors} floors`;
   }
 }
+
+export default SkyHighBuilding;
