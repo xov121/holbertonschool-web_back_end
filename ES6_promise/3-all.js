8adaa8e12f8a308789817b0c336e8d54d5cb8431
xov121 <x.o.v121@gmail.com>
2024-05-27 08:50:50 -0400

    Task 3

---
+++ ES6_promise/3-all.js
@@ -0,0 +1,16 @@
+#!/usr/bin/node
+
+import { uploadPhoto, createUser } from './utils.js';
+
+function handleProfileSignup() {
+  return Promise.all([uploadPhoto(), createUser()])
+    .then((results) => {
+      const [photo, user] = results;
+      console.log(`${photo.body} ${user.firstName} ${user.lastName}`);
+    })
+    .catch(() => {
+      console.log('Signup system offline');
+    });
+}
+
+export default handleProfileSignup;
