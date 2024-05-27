5ca6e1668fb18f9e9d38e712aa7b08c2ad4d6d1f
xov121 <x.o.v121@gmail.com>
2024-05-27 08:58:51 -0400

    Task 6

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -1,24 +1,33 @@
-#!/usr/bin/node
-
 import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
-export default function handleProfileSignup(firstName, lastName, fileName) {
-  const userPromise = signUpUser(firstName, lastName).then(value => ({
-    status: 'fulfilled',
-    value,
-  })).catch(error => ({
-    status: 'rejected',
-    value: error.toString(),
-  }));
+async function handleProfileSignup(firstName, lastName, fileName) {
+  const promise1 = {
+    status: 'pending ',
+  };
+  const promise2 = {
+    status: 'pending ',
+  };
 
-  const photoPromise = uploadPhoto(fileName).then(value => ({
-    status: 'fulfilled',
-    value,
-  })).catch(error => ({
-    status: 'rejected',
-    value: error.toString(),
-  }));
+  try {
+    const signup = await signUpUser(firstName, lastName);
+    promise1.status = 'fulfilled';
+    promise1.value = signup;
+  } catch (err) {
+    promise1.status = 'rejected';
+    promise1.value = err.toString();
+  }
 
-  return Promise.allSettled([userPromise, photoPromise]);
+  try {
+    const upload = await uploadPhoto(fileName);
+    promise2.status = 'fulfilled';
+    promise2.value = upload;
+  } catch (err) {
+    promise2.status = 'rejected';
+    promise2.value = err.toString();
+  }
+
+  return [promise1, promise2];
 }
+
+export default handleProfileSignup;
