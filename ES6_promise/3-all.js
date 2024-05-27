5d10736755ad803b6230914c8562f86583677c99
xov121 <x.o.v121@gmail.com>
2024-05-27 08:52:59 -0400

    Task 3

--- ES6_promise/3-all.js
+++ ES6_promise/3-all.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 
-import { uploadPhoto, createUser } from './utils.js';
+import { uploadPhoto, createUser } from './utils';
 
 function handleProfileSignup() {
   return Promise.all([uploadPhoto(), createUser()])
