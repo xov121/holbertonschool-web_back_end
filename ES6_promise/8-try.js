e66a840f9fe2814a639191f9931a622200d4a05c
xov121 <x.o.v121@gmail.com>
2024-05-27 09:21:16 -0400

    Task 8

---
+++ ES6_promise/8-try.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+
+export default function divideFunction(numerator, denominator) {
+  if (denominator === 0) {
+    throw new Error('cannot divide by 0');
+  } else {
+    return numerator / denominator;
+  }
+}
